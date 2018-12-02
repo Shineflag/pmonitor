8244fc9e9ada535d5ddf72291fcd9b1c889ae729
AmyLuo <283263775@qq.com>
2018-12-02 11:59:33 +0800

    更改服务器名称

--- etc/conf/pmonitor_conf.lua
+++ etc/conf/pmonitor_conf.lua
@@ -5,13 +5,13 @@
 local gdir="/data/dp/run"
 -- local gdir="/deploy/server/PokerGo/run"
 return {
-	{name="德州进程", pname="CZTexasServer",        pnum=1,   dir=gdir,         sh="run_texas.sh"},
-	{name="金币进程", pname="CZMoneyServer",        pnum=1,   dir=gdir ,        sh="run_money.sh"},
-	{name="比赛列表", pname="CZMatchListServer",    pnum=1,   dir=gdir,         sh="run_matchList.sh"},
-	{name="接入进程", pname="CZAccessServer",       pnum=1,   dir=gdir,         sh="run_access.sh"},
-	{name="俱乐部",   pname="CZClubServer",         pnum=1,   dir=gdir,         sh="run_club.sh"},
-	{name="游戏日志", pname="CZGamelogServer",      pnum=1,   dir=gdir,         sh="run_gamelog.sh"},
-	{name="通知服",   pname="CZNotifyServer",       pnum=1,   dir=gdir,         sh="run_notify.sh"},
-	{name="状态服",   pname="CZStateServer",        pnum=1,   dir=gdir,         sh="run_state.sh"},
-	{name="统计服",   pname="CZStatisticServer",    pnum=1,   dir=gdir,         sh="run_statistic.sh"},
+	{name="Texas", pname="CZTexasServer",        pnum=1,   dir=gdir,         sh="run_texas.sh"},
+	{name="Money", pname="CZMoneyServer",        pnum=1,   dir=gdir ,        sh="run_money.sh"},
+	{name="Match", pname="CZMatchListServer",    pnum=1,   dir=gdir,         sh="run_matchList.sh"},
+	{name="Access", pname="CZAccessServer",       pnum=1,   dir=gdir,         sh="run_access.sh"},
+	{name="Club",   pname="CZClubServer",         pnum=1,   dir=gdir,         sh="run_club.sh"},
+	{name="Log", pname="CZGamelogServer",      pnum=1,   dir=gdir,         sh="run_gamelog.sh"},
+	{name="Notify",   pname="CZNotifyServer",       pnum=1,   dir=gdir,         sh="run_notify.sh"},
+	{name="State",   pname="CZStateServer",        pnum=1,   dir=gdir,         sh="run_state.sh"},
+	{name="Statis",   pname="CZStatisticServer",    pnum=1,   dir=gdir,         sh="run_statistic.sh"},
 } 
